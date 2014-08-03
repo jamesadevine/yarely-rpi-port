0b38f0b936fb0e3a93b1e3d30171f5452bc49e54
gunmania0 <james@devine.eu>
2014-08-03 23:10:14 +0100

    Added logging to cachemanager

--- yarely/frontend/linux/helpers/cachemanager.py
+++ yarely/frontend/linux/helpers/cachemanager.py
@@ -1,5 +1,6 @@
 __author__ = 'James'
 from subprocess import Popen
+import logging
 from multiprocessing import Process,Manager
 from yarely.frontend.linux.helpers.assetwrappers import \
     ImageAsset, BrowserAsset, PlayerAsset
@@ -14,13 +15,15 @@ import os
 import time
 from subprocess import check_output
 
+log = logging.getLogger(__name__)
+
 xvfb_run_bin='/usr/bin/xvfb-run'
 URL_REFRESH_TIME=1800 #30 minutes
 FILE_DELETION_TIME=1800 #30 minutes
 
 class cache_manager(object):
     def __init__(self,cache_path):
-        print('Initialising Cache Manager')
+        log.info('Initialising Cache Manager')
         self.cache_path=cache_path
         self.manager=Manager()
         if not os.path.exists('/tmp/downloading/'+self.cache_path):
@@ -44,11 +47,13 @@ class cache_manager(object):
         self.width=self.width_height.split('x')[0]
         self.height=self.width_height.split('x')[1]
 
+        log.info('Initialising url removal')
         #start url removal and file deletion processes.
         url_removal_process=Process(target=self.remove_old_items())
         url_removal_process.daemon=True
         url_removal_process.start()
 
+        log.info('Initialising old file deletion')
         file_deletion_process=Process(target=self.remove_old_items())
         file_deletion_process.daemon=True
         file_deletion_process.start()
@@ -92,29 +97,22 @@ class cache_manager(object):
                 #download file in background without blocking main thread
                 if isinstance(asset,BrowserAsset) and not self.is_in_active_downloads(content_src_uri) and self.running_process.value<5:
                     self.increment_running_process()
-                    print('downloading browser asset')
+                    log.info('downloading browser asset')
                     self.add_to_active_downloads(content_src_uri)
                     #if it's a page we use popen to save a screencap of that image
                     self.download_page(content_src_uri,cache_path,self.active_downloads_lock,)
                 elif not self.is_in_active_downloads(content_src_uri) and self.running_process.value<5:
                     self.increment_running_process()
-                    print('downloading image or video asset')
+                    log.info('downloading image or video asset')
                     self.add_to_active_downloads(content_src_uri)
 
                     #else use a traditional write buffer to download the resource.
                     process=Process(target=self.download_resource,args=(content_src_uri,cache_path,self.active_downloads_lock,))
                     process.daemon=True
                     process.start()
-                elif self.is_in_active_downloads(content_src_uri):
-                    self.cache_path
-                    #print('IN ACTIVE DOWNLOADS ASDJAKDHKSAHDFJLKASHDFJALSHDFJHASDLFJKHALSDKJ')
-                #else:
-                    #print('Too many processes are running')
-                return None
         return None
 
     def decrement_running_process(self):
-        print('DECREMENTING RUNNING')
         self.running_process_lock.acquire()
         print(str(self.running_process.value))
         self.running_process.value-=1
@@ -122,7 +120,6 @@ class cache_manager(object):
         self.running_process_lock.release()
 
     def increment_running_process(self):
-        print('Incrementing RUNNING')
         self.running_process_lock.acquire()
         print(str(self.running_process.value))
         self.running_process.value+=1
@@ -145,35 +142,35 @@ class cache_manager(object):
     def download_resource(self, uri,download_path,active_downloads_lock):
         #store resource in separate directory so that the scheduler doesn't try and read a bad resource...
         temp_path='/tmp/downloading'+download_path
-        print('downloading to '+temp_path)
-        print('downloading from: '+uri)
+        log.info('downloading to '+temp_path)
+        log.info('downloading from: '+uri)
         try:
             filehandle = urlopen(uri)
             open(temp_path, 'wb').write(filehandle.read())
-            print('Download complete - moving to actual cache: '+download_path)
+            log.info('Download complete - moving to actual cache: '+download_path)
             os.rename(temp_path,download_path)
             active_downloads_lock.acquire()
             del self.active_downloads[uri]
             active_downloads_lock.release()
-            print('decrementing from process')
+            log.info('decrementing from process')
             self.decrement_running_process()
         except (URLError, IOError) as e:
-            print('Failed to fetch resource from url: '+str(e))
-            print('decrementing from process')
+            log.info('Failed to fetch resource from url: '+str(e))
+            log.info('decrementing from process')
             self.decrement_running_process()
 
     def download_page(self, uri,download_path,active_downloads_lock):
         #store image of page in separate directory so that the scheduler doesn't try and read a bad resource...
         temp_path='/tmp/downloading'+download_path
-        print('downloading to '+temp_path)
+        log.info('downloading to '+temp_path)
         process=Popen(['xvfb-run','-e','/dev/stdout','-s -screen 0, '+str(self.width_height)+'x24', 'cutycapt', '--url='+uri,'--min-width='+str(self.height), '--min-height='+str(self.height),'--out='+temp_path,'--delay=7000'])
         process.wait()
-        print('Download complete - moving to actual cache: '+download_path)
+        log.info('Download complete - moving to actual cache: '+download_path)
         os.rename(temp_path,download_path)
         active_downloads_lock.acquire()
         del self.active_downloads[uri]
         active_downloads_lock.release()
-        print('decrementing from process')
+        log.info('decrementing from process')
         self.decrement_running_process()
 
     #this function runs in the background and removes 20 items whenever it sees that the diskspace usage >20%
